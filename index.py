40eaae18fe9fd4c37750d1079ded28f91da7a7e1
Onyekachi Nwakaihe <onyekachi.nwakaihe@africaprudential.com>
2023-06-18 05:47:17 +0100

    Added web url and scrape the title element of the page

--- index.py
+++ index.py
@@ -1,6 +1,18 @@
 import requests
+from bs4 import BeautifulSoup
 
 URL = "https://books.toscrape.com/"
 page = requests.get(URL)
 
-print(page.text)
+soup = BeautifulSoup(page.content, "html.parser")
+
+results = soup.find("ol", class_="row")
+
+# print(results.prettify())
+
+book_elements = results.find_all("article", class_="product_pod")
+
+for book_element in book_elements:
+    title_element = book_element.find("h3")
+    print(title_element.text)
+    print()
